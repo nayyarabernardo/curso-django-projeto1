e5bb90f93f13fcda40eff34527b9cec34e3e5b98
Nayara <nayyarabernardo@gmail.com>
2023-06-01 20:14:25 -0300

    Aula 24

---
+++ recipes/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+# from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+
+
+def contato(request):
+    return HttpResponse('contato 2')
+
+
+def sobre(request):
+    return HttpResponse('sobre 2')
